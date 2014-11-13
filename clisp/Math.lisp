59a67e4ace7f4e02648f44149c4e73ef35d2ed4a
Jim Capozzoli <saltmiser@gmail.com>
2014-11-13 01:09:16 -0500

    Added permutation function.

--- clisp/Math.lisp
+++ clisp/Math.lisp
@@ -3,3 +3,10 @@
   (if (= n 0)
       1
       (* n (factorial (- n 1)))))
+
+(defun permutation (n k r)
+  "The stlxl.Math.permutation function accepts n, k and r in that order.  R is a boolean, k are the choices, and n is the total number of options., n choose k."
+  (if r
+      (expt n k)
+      (/ (factorial n) (factorial (- n k)))))
+
